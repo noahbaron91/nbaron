01a4718ec1ffc9c72893a8fbc6e26d7ce1fa3472
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-08-30 10:54:30 -0700

    feat: improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 function Header() {
   return (
     <div className='px-8 py-4'>
-      <h1 className='font-medium text-xl'>nbaron</h1>
+      <a className='font-medium text-xl' href='/'>
+        nbaron
+      </a>
     </div>
   );
 }
@@ -10,12 +12,38 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <p className='text-center'>
-        making the internet fun again.{' '}
-        <a href='/cofounder' className='text-[#0000FF]'>
-          looking for cofounders
-        </a>
-      </p>
+      <section className='mx-8 md:mx-auto sm:mx-24 md:max-w-[700px] flex flex-col gap-3'>
+        <p>
+          Hi, I{"'"}m Noah! I{"'"}m a software engineer and designer. I like to
+          build things that are fun, useful, and make the world a better place
+        </p>
+        <div className='flex flex-col gap-1'>
+          <h3 className='font-bold'>Projects:</h3>
+          <ul className='list-disc pl-8'>
+            <li>
+              A website you can only visit once:{' '}
+              <a
+                className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'
+                href='https://onlyvisitonce.com'
+                target='_blank'
+              >
+                onlyvisitonce.com
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <p>
+          enjoy my work?{' '}
+          <a
+            className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'
+            href='https://buymeacoffee.com/noahbaron'
+            target='_blank'
+          >
+            buy me a coffee!
+          </a>
+        </p>
+      </section>
     </main>
   );
 }
