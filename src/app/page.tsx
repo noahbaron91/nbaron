493322110049fe1675aab26b066b956cfb8f0bf6
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-09-06 13:28:55 -0700

    feat: revert text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,8 @@ export default function Home() {
       <Header />
       <section className='mx-8 md:mx-auto sm:mx-24 md:max-w-[700px] flex flex-col gap-3'>
         <p>
-          Hi, I{"'"}m Noah! I{"'"}m a software engineer and designer. <br />I
-          like to build things that are fun, useful, and make the world a better
-          place.
+          Hi, I{"'"}m Noah! I{"'"}m a software engineer and designer. I like to
+          build things that are fun, useful, and make the world a better place
         </p>
         <div className='flex flex-col gap-1'>
           <h3 className='font-bold'>Projects:</h3>
@@ -46,9 +45,7 @@ export default function Home() {
         </div>
 
         <p>
-          I have more ambitious ideas I want to build, but{' '}
-          <strong>I{"'"}m simply lacking the funds to create them.</strong> If
-          you like what I{"'"}m doing and want to help me out, you can{' '}
+          If you like what I{"'"}m doing and want to support me, you can{' '}
           <a
             className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'
             href='https://buymeacoffee.com/noahbaron'
